5d4152875f0ae322200f537db4a2b07a7f73d15f
balavignesh-s <balavignesh@shippable.com>
2016-01-18 14:12:00 +0530

    modified print syntax so that is is compatible with all python versions

--- sample.py
+++ sample.py
@@ -4,4 +4,4 @@ import os, memcache
 memcached_port = 11211
 memcached = memcache.Client(["127.0.0.1:{0}".format(memcached_port)])
 memcached.set("name", "Shippable Memcached Demo")
-print memcached.get("name")
+print(memcached.get("name"))
